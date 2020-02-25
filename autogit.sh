a0bd1de37d640b237827e03c7de6d673b7c498f9
Danilo Pianini <danilo.pianini@unibo.it>
2020-02-25 12:56:05 +0100

    [skip ci] Add autogit

---
+++ autogit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for file in data/*
+  do LASTLINE="$(tail -n 1 $file)"
+  if [ "${LASTLINE:0:1}" == "#" ]
+    then git add -f "${file}"
+  fi
+done
+COMMIT_MESSAGE="[AUTOMATIC ${USER}@${HOSTNAME}] - `date --iso-8601=minutes` data update"
+git commit -m "${COMMIT_MESSAGE}"
+git fetch
+git merge FETCH_HEAD -m "$COMMIT_MESSAGE (merge commit)"
+git commit  -m "$COMMIT_MESSAGE (merge commit)"
+git push
