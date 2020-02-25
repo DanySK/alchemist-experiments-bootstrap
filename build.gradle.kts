1c2f3df135302e9769e4d5b51c409db51722431f
Danilo Pianini <danilo.pianini@unibo.it>
2020-02-25 10:48:56 +0100

    Update configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-/*
- * DEFAULT GRADLE BUILD FOR ALCHEMIST SIMULATOR
- */
+import java.io.ByteArrayOutputStream
 
 plugins {
     application
+    kotlin("jvm") version "1.3.61"
 }
 
 repositories {
@@ -26,28 +25,53 @@ sourceSets {
     }
 }
 dependencies {
-    // it is highly recommended to replace the '+' symbol with a fixed version
-    implementation("it.unibo.alchemist:alchemist:+")
+    implementation("it.unibo.alchemist:alchemist:9.3.0")
+    implementation(kotlin("stdlib-jdk8"))
 }
+
+// Heap size estimation for batches
+val maxHeap: Long? by project
+val heap: Long = maxHeap ?:
+if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+    ByteArrayOutputStream().use { output ->
+        exec {
+            executable = "bash"
+            args = listOf("-c", "cat /proc/meminfo | grep MemAvailable | grep -o '[0-9]*'")
+            standardOutput = output
+        }
+        output.toString().trim().toLong() / 1024
+    }
+        .also { println("Detected ${it}MB RAM available.") }  * 9 / 10
+} else {
+    // Guess 16GB RAM of which 2 used by the OS
+    14 * 1024L
+}
+val taskSizeFromProject: Int? by project
+val taskSize = taskSizeFromProject ?: 512
+val threadCount = maxOf(1, minOf(Runtime.getRuntime().availableProcessors(), heap.toInt() / taskSize ))
 
 val alchemistGroup = "Run Alchemist"
 /*
  * This task is used to run all experiments in sequence
  */
-val runAll by tasks.register<DefaultTask>("runAll") {
+val runAllGraphic by tasks.register<DefaultTask>("runAllGraphic") {
     group = alchemistGroup
-    description = "Launches all simulations"
+    description = "Launches all simulations with the graphic subsystem enabled"
+}
+val runAllBatch by tasks.register<DefaultTask>("runAllBatch") {
+    group = alchemistGroup
+    description = "Launches all experiments"
 }
 /*
  * Scan the folder with the simulation files, and create a task for each one of them.
  */
 File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
-    .filter { it.extension == "yml" }
-    .sortedBy { it.nameWithoutExtension }
-    .forEach {
-        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.capitalize()}") {
+    ?.filter { it.extension == "yml" }
+    ?.sortedBy { it.nameWithoutExtension }
+    ?.forEach {
+        fun basetask(name: String, additionalConfiguration: JavaExec.() -> Unit = {}) = tasks.register<JavaExec>(name) {
             group = alchemistGroup
-            description = "Launches simulation ${it.nameWithoutExtension}"
+            description = "Launches graphic simulation ${it.nameWithoutExtension}"
             main = "it.unibo.alchemist.Alchemist"
             classpath = sourceSets["main"].runtimeClasspath
             args(
@@ -55,10 +79,26 @@ File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
                 "-g", "effects/${it.nameWithoutExtension}.aes"
             )
             if (System.getenv("CI") == "true") {
-                args("-hl", "-t", "10")
+                args("-hl", "-t", "2")
             }
+            this.additionalConfiguration()
         }
-        // task.dependsOn(classpathJar) // Uncomment to switch to jar-based cp resolution
-        runAll.dependsOn(task)
+        val capitalizedName = it.nameWithoutExtension.capitalize()
+        val graphic by basetask("run${capitalizedName}Graphic")
+        runAllGraphic.dependsOn(graphic)
+        val batch by basetask("run${capitalizedName}Batch") {
+            description = "Launches batch experiments for $capitalizedName"
+            jvmArgs("-XX:+AggressiveHeap")
+            maxHeapSize = "${minOf(heap.toInt(), Runtime.getRuntime().availableProcessors() * taskSize)}m"
+            File("data").mkdirs()
+            args(
+                "-e", "data/${it.nameWithoutExtension}",
+                "-b",
+                "-var", "seed", "speed", "meanNeighbors", "nodeCount",
+                "-p", threadCount,
+                "-i", 1
+            )
+        }
+        runAllBatch.dependsOn(batch)
     }
 
