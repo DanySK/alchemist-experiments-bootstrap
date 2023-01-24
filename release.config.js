0311e56a5110d883c2c71a8a7002445fdbf9adf6
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-24 15:11:04 +0100

    fix(release): push the updated files after version calculation

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ echo version=\${nextRelease.version} > gradle.properties
 echo VERSION="\${nextRelease.version}" > .env
 echo PROJECT_NAME=$(grep -Po 'rootProject\\s*\\.\\s*name\\s*=\\s*"\\K[\\w-]+(?=")' settings.gradle.kts) >> .env
 git commit -a -m "chore(release): update version descriptors to \${nextRelease.version}"
+git push
 docker compose build
 `
 var publishCmd = `
