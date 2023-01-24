4c7d8495adfac26818aa87cab08cc25f954d59aa
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-24 14:10:56 +0100

    fix(release): publish the images built in CI

--- release.config.js
+++ release.config.js
@@ -2,6 +2,8 @@ var publishCmd = `
 echo version=\${nextRelease.version} > gradle.properties
 echo VERSION="\${nextRelease.version}" > .env
 echo PROJECT_NAME=$(grep -Po 'rootProject\\s*\\.\\s*name\\s*=\\s*"\\K[\\w-]+(?=")' settings.gradle.kts) >> .env
+docker compose build
+docker compose push
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
