c1851619d8e302165c6eaeaf66ae52c561e1a54c
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-29 16:18:52 +0100

    fix(release): move pushes with refreshed versions to the publish phase

--- release.config.js
+++ release.config.js
@@ -2,12 +2,13 @@ var prepareCmd = `
 echo version=\${nextRelease.version} > gradle.properties
 echo VERSION="\${nextRelease.version}" > .env
 echo PROJECT_NAME=$(grep -Po 'rootProject\\s*\\.\\s*name\\s*=\\s*"\\K[\\w-]+(?=")' settings.gradle.kts) >> .env
-git commit -a -m "chore(release): \${nextRelease.version} [skip ci]"
-git push
 docker compose build
 `
 var publishCmd = `
 docker compose push
+git add gradle.properties .env
+git commit -m "chore(release): update gradle.properties .env versions to \${nextRelease.version} [skip ci]"
+git push
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
