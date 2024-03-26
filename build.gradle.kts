7ce5f7067e279391a9db900948a39daec24753da
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-03-26 15:49:58 +0100

    build: use the threadCount variable in the batch tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,6 +115,17 @@ File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
             description = "Launches batch experiments for $capitalizedName"
             maxHeapSize = "${minOf(heap.toInt(), Runtime.getRuntime().availableProcessors() * taskSize)}m"
             File("data").mkdirs()
+            args("--override",
+                """
+                    launcher: {
+                        parameters: {
+                            batch: [ seed, spacing, error ],
+                            showProgress: true,
+                            autoStart: true,
+                            parallelism: $threadCount,
+                        }
+                    }
+                """.trimIndent())
         }
         runAllBatch.dependsOn(batch)
     }
